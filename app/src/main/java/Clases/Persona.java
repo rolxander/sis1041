8dafbd6f77b1b34a6109254c051faf2b24e9f67d
Rolaxx <gaelelpapi@gmail.com>
2019-11-22 08:39:56 -0400

    jaja

---
+++ app/src/main/java/Clases/Persona.java
@@ -0,0 +1,21 @@
+package Clases;
+
+public class Persona {
+    public String nombre;
+    public String apellido;
+    public String edad;
+    public String longitud;
+    public String latitud;
+    public  Persona(){
+
+    };
+    public Persona(String nombre,String apellido,String edad,String altura,String latitud,String longitud){
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.longitud = longitud;
+        this.latitud = latitud;
+    }
+
+}
